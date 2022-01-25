91006965b149926dda0898a7a19966db892ee784
LoaiEl <luaielkhuffash@gmail.com>
2022-01-25 13:28:42 +0400

    Added Organizer

--- Organize script.py
+++ Organize script.py
@@ -1,4 +1,5 @@
 # file organizer for windows with python 
+from importlib.resources import path
 import time 
 import shutil
 import os
@@ -7,11 +8,14 @@ import os
 pathFrom = 'C:\\Users\\Administrator\\Downloads\\'
 
 # Check the following folders if they exist and can make them
-folders = ['Videos','Documents', 'Images', 'Zip Files']
+folders = ['Videos','Documents', 'Images', 'Zip Files','Text Files']
+
+
 
 
 def begin():
     checkFileDestinationPath()
+    organizeByExt()
 
 def checkFileDestinationPath():
     for a in folders:
@@ -21,5 +25,16 @@ def checkFileDestinationPath():
 
         else: print("File already exists")
 
-     
-begin()
+def organizeByExt():
+    files = os.listdir(pathFrom)
+    for a in files:
+        if (a.endswith("jpg")):
+            shutil.move(pathFrom + a, pathFrom + "Images\\")
+        if (a.endswith("jpeg")):
+            shutil.move(pathFrom + a, pathFrom + "Images\\")
+        if (a.endswith("png")):
+            shutil.move(pathFrom + a, pathFrom + "Images\\")
+        if (a.endswith("gif")):
+            shutil.move(pathFrom + a, pathFrom + "Images\\")
+            
+organizeByExt()
